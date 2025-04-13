cade2e6a9bbb1b1a3cf92be54cefcb16fbc78181
wintun-devop <wintun.hlaing@outlook.com>
2025-04-13 16:16:56 +0630

    init

---
+++ app/home/component/Home.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const HomePage = () => {
+    return (
+        <>
+        <h4>Home page</h4>
+        </>
+    )
+}
+
+export default HomePage;
---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+import HomePage from "./component/Home"
+
+export default function Home() {
+    return (
+        <div className='h-screen w-full'>
+            <HomePage />
+        </div>
+    )
+}
